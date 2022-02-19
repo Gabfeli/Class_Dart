0029333a8166f30df77d453b99e7e8f9a79521ab
GAB FELIX <93562369+Gabfeli@users.noreply.github.com>
2022-02-18 21:11:01 -0300

    Update televisao.dart

--- Aula sobre Class/televisao.dart
+++ Aula sobre Class/televisao.dart
@@ -1,52 +1,47 @@
 class Televisao {
   bool? status;
-  int? canal;
+  int canal = 0;
   int volume = 0;
 
-  Televisao(){
-   this.status==false; 
+  Televisao() {
+    this.status == false;
   }
-  
+
   void LigaDesliga() {
-    if(!this.status == false){
-    this.status == true;
-      "" 3;
-      "" 10;
-    }
-    else{
-    this.status = false;
+    if (this.status == false) {
+      this.status == true;
+      "";
+      3;
+      "";
+      10;
+    } else {
+      this.status = false;
     }
   }
 
   void AumentaCanal() {
-   if(this.status){
-   this.canal ++;
-   }
+    if (this.status!) {
+      this.canal++;
+    }
   }
 
   void AumentaVolume() {
-   if(this.status){
-   this.Volume ++;
-   }
-  }
-  
-  void DiminuirVolume() {
-   if(this.status){
-   this.Volume = Volume - 1;
-   }
+    if (this.status!) {
+      this.volume++;
+    }
   }
 
-  int mostraCanal() {
+  void DiminuirVolume() {
+    if (this.status!) {
+      this.volume = volume - 1;
+    }
+  }
+
+  int MostraCanal() {
     return this.canal;
   }
 
-  int mostraVolume() {
-    return volume;
+  int MostraVolume() {
+    return this.volume;
   }
-}
-
-void AumentaCanal() {
-if(this.canal){
-  this.canal ++;
-}
 }
